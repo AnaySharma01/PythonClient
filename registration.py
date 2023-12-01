d570b971c5d0cb121d359378fa00507bda4b6c5a
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-12-01 07:58:39 -0500

    Create registration.py

---
+++ registration.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+from tkinter import messagebox
+
+root = tk.Tk()
+root.configure(bg = '#639c8f')
+root.geometry('500x500')
+root.title('Python Client')
+root.resizable(False,False)
+
+
+
+root.mainloop()
