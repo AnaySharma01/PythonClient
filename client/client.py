8780db1547f2dda0156792050b803d6d49d830e5
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-12-04 23:05:49 -0500

    Update client.py

--- client/client.py
+++ client/client.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import font
 from tkinter import messagebox
-from loginPage import *
+from clientLogin import *
 
 GUI = tk.Tk()
 GUI.configure(bg = '#639c8f')
