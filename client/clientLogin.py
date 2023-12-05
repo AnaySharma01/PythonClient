c41a080bcc7d570e0140c1746cdf8206f07cb44d
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-12-04 22:51:48 -0500

    Update clientLogin.py

--- client/clientLogin.py
+++ client/clientLogin.py
@@ -4,32 +4,34 @@ from tkinter import ttk
 from tkinter import font
 from tkinter import messagebox
 
-root = tk.Tk()
-root.configure(bg = '#639c8f')
-root.geometry('500x500')
-root.title('Python Client')
-root.resizable(False,False)
 
-e1 = StringVar()
-e2 = StringVar()
+def loginWindow():
+    root = tk.Tk()
+    root.configure(bg = '#639c8f')
+    root.geometry('500x500')
+    root.title('Python Client')
+    root.resizable(False,False)
 
-username = Label(root,text="Enter your username: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
-username.grid(row = 0, column = 0)
+    e1 = StringVar()
+    e2 = StringVar()
 
-password = Label(root,text="Enter your password: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
-password.grid(row = 1, column = 0)
+    username = Label(root,text="Enter your username: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
+    username.grid(row = 0, column = 0)
 
-username = Entry(root,textvariable = e1)
-username.grid(row = 0, column = 1)
+    password = Label(root,text="Enter your password: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
+    password.grid(row = 1, column = 0)
 
-password = Entry(root,textvariable = e2)
-password.grid(row = 1, column = 1)
+    username = Entry(root,textvariable = e1)
+    username.grid(row = 0, column = 1)
 
-def save():
-  e1.get()
-  e2.get()
+    password = Entry(root,textvariable = e2)
+    password.grid(row = 1, column = 1)
+
+    def save():
+      e1.get()
+      e2.get()
   
-save_everything = Button(root,text="Save",command=save, bg='#fcba03',font = 'bold')
-save_everything.grid(row = 500, column = 0)
+    save_everything = Button(root,text="Save",command=save, bg='#fcba03',font = 'bold')
+    save_everything.grid(row = 500, column = 0)
 
-root.mainloop()
+    root.mainloop()
