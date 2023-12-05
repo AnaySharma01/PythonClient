3da214a87840f1ffe846181abdb31cd6e46c3ff0
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-12-04 21:35:14 -0500

    Update client_homepage.py

--- client/client_homepage.py
+++ client/client_homepage.py
@@ -7,35 +7,35 @@ import main
 import clientGUI
 import homePage
 
-clientLogin = tk.Tk()
-clientLogin.configure(bg = '#639c8f')
-clientLogin.geometry('500x500')
-clientLogin.title('Python Client')
-clientLogin.resizable(False,False)
+clientHomepage = tk.Tk()
+clientHomepage.configure(bg = '#639c8f')
+clientHomepage.geometry('500x500')
+clientHomepage.title('Python Client')
+clientHomepage.resizable(False,False)
 
 e1 = StringVar()
 e2 = StringVar()
 
-username = Label(clientLogin,text="Enter your username: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
+username = Label(clientHomepage,text="Enter your username: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
 username.grid(row = 0, column = 0)
 
-password = Label(clientLogin,text="Enter your password: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
+password = Label(clientHomepage,text="Enter your password: ",bg = '#639c8f',fg='#e21d76',font = 'bold')
 password.grid(row = 1, column = 0)
 
-username = Entry(clientLogin,textvariable = e1)
+username = Entry(clientHomepage,textvariable = e1)
 username.grid(row = 0, column = 1)
 
-password = Entry(clientLogin,textvariable = e2)
+password = Entry(clientHomepage,textvariable = e2)
 password.grid(row = 1, column = 1)
 
 def save():
   e1.get()
   e2.get()
 
-save_everything = Button(clientLogin,text="Save",command=save, bg='#fcba03',font = 'bold')
+save_everything = Button(clientHomepage,text="Save",command=save, bg='#fcba03',font = 'bold')
 save_everything.grid(row = 500, column = 0)
 
-logout = tk.Button(clientLogin, text = 'Logout', bg='#A877BA',font = 'bold', command = clientLogin.destroy)
-clientLogin.grid(row = 20, column = 12)
+logout = tk.Button(clientHomepage, text = 'Logout', bg='#A877BA',font = 'bold', command = clientHomepage.destroy)
+clientHomepage.grid(row = 20, column = 12)
 
-clientLogin.mainloop()
+clientHomepage.mainloop()
